1d5e326d4fa049364dbd6dfcfa8c80ec88a75fa9
stenio-fonteles <macacovelho.ss@gmail.com>
2022-08-16 18:16:10 -0300

    configuração de rotas feitas

--- src/components/detailsMovie/index.jsx
+++ src/components/detailsMovie/index.jsx
@@ -1,12 +1,15 @@
 import { useParams } from "react-router-dom"
 import axios from 'axios'
 import { useEffect, useState } from "react";
-import { Section,Perfil } from "./styled";
+import { Section,Perfil,Genres,Button,Grid } from "./styled";
 export default function DetailsMovies(){
     const {id} = useParams();
     const [datas,setDatas] = useState([])
     const [background,setBackground] = useState('')
     const [image, setImage] = useState('')
+    const[genres,setGenres] = useState([])
+    const [grid,setGrid] = useState([])
+    const [site,setSite] = useState('')
     async function getMovies(){
         const {data} = await axios.get(`https://api.themoviedb.org/3/movie/${id}?api_key=758d90bb10802747712fd860965c0320&append_to_response=videos,images`)
         setDatas(data)
@@ -15,12 +18,34 @@ export default function DetailsMovies(){
     setTimeout(()=>{
         setBackground(`https://image.tmdb.org/t/p/original${datas.backdrop_path}`)
         setImage(`https://image.tmdb.org/t/p/original${datas.poster_path}`)
+        setGenres(datas.genres)
+        setGrid(datas.images.posters)
+        setSite(datas.homepage)
     },300)
+
+    // console.log(genres)
     
     useEffect(()=>{getMovies()},[])
     return(
         <Section background={background}> 
+        <div>
             <Perfil src={image}/>
+            <Genres>
+                {genres.map((genero) =>{
+                    return(
+                        <Button>{genero.name}</Button>
+                    )
+                })}
+            </Genres>
+        </div>
+            <a href={site}>Visitar site oficial</a>
+        <Grid>
+            {grid.map((i) =>{
+                return(
+                    <img src={`https://image.tmdb.org/t/p/original${i.file_path}`}/>
+                    )
+                })}
+        </Grid>
         </Section>
     )
 }
--- src/components/detailsMovie/styled.js
+++ src/components/detailsMovie/styled.js
@@ -7,13 +7,47 @@ export const Section = styled.div`
    background-position: center;
    background-size: cover;
    box-shadow: inset -10px -100vh 30px 30px  rgba(0, 0, 0, 0.8);
+   align-items: center;
+   text-align: center;
+  
 `
 export const Perfil = styled.img`
     width: 500px;
     height: auto;
-    top: 50%;
-    position: absolute;
-    left: 50%;
-    transform: translate(-50%,-50%);
+    margin-top: 50px;
     border-radius:30px;
 ` 
+export const Genres = styled.div`
+    //display: flex;
+    
+
+`
+
+export const Button = styled.button`
+    padding: 10px 30px;
+    color: white;
+    background-color: red;
+    border: none;
+    outline: none;
+    margin-right: 5px;
+    &:hover{
+        background-color: orange;
+        color: black;
+
+    }
+`
+
+export const Grid = styled.div`
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr 1fr;
+    gap: 10px;
+    width: 100%;
+    align-items: center;
+    justify-items: center;
+    margin-top: 5%;
+    img{
+        width: 300px;
+        border-radius: 20px;
+    }
+
+`
