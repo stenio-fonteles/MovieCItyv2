a4cc7ccefe7a4caf63004940d14f9eaf792dc652
stenio-fonteles <macacovelho.ss@gmail.com>
2022-08-17 20:10:46 -0300

    user e senha salvando com sucesso

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,20 +1,25 @@
 import axios from "axios"
-
+import { useState } from "react"
+import { Form } from "./styled"
 export default function Login() {
+    const [user,setUser] = useState('')
+    const [password,setPassword] = useState('')
    function teste(){
         axios.post('http://localhost:3000/Login',
         {
-            Name: 'Fred',
-            PassWord: 'Flintstone'
+            Name: user,
+            PassWord:password 
         })
    } 
-   teste()
+
     return(
-        <>
-            <input alt="Nome" placeholder="nome"> Nome </input>
-            <input> Password</input>
-            oi
-        </>
+        <Form>
+            <label for="nome">Nome:</label>
+            <input type={'text'} id="nome" onChange={(e)=>{setUser(e.target.value)}} />
+            <label for="nome">Password:</label>
+            <input type={"password"} onChange={(e) =>{ setPassword(e.target.value)}}/>
+            <button onClick={()=>{teste()}}> Salvar </button>
+        </Form>
     )
 
 }
